1a373f0d2e475c0355197403e6ef6f0acff0e174
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-01-13 23:58:41 +0530

    Created controllers for routes

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+exports.authentication = (req, res) => {};
+
+exports.addition = (req, res) => {};
+
+exports.subtraction = (req, res) => {};
+
+exports.multiplication = (req, res) => {};
+
+exports.division = (req, res) => {};
+
+exports.history = (req, res) => {};
