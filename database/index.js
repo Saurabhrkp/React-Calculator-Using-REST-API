7a1fa88256e2a4a80b1adf02237c1e4c151ecd27
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-01-13 23:57:43 +0530

    Added database pool

---
+++ database/index.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_PASSWORD,
+  port: process.env.MYSQL_PORT,
+});
+
+module.exports = pool.promise();
