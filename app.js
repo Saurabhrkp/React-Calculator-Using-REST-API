6045e6b8a97d6dac2e7badd96f4c2d18a31c6623
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-01-14 14:42:05 +0530

    Rename client folder path

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use('/', indexRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('react-client/build'));
+  app.use(express.static('./react-client/build'));
   app.get('*', (req, res) => {
     res.sendFile(
       path.resolve(__dirname, 'react-client', 'build', 'index.html')
