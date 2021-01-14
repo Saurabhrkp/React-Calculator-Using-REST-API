72df8612dc35126f088c9d1e09b1c4f6949d9587
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-01-14 10:10:53 +0530

    Added authentication and checks for values as middleware

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,34 @@ const express = require('express');
 const router = express.Router();
 const indexController = require('../controllers/index');
 
-router.get('/add', indexController.addition);
+router.get(
+  '/add',
+  indexController.authentication,
+  indexController.checkForValueInQuery,
+  indexController.addition
+);
 
-router.get('/subtract', indexController.subtraction);
+router.get(
+  '/subtract',
+  indexController.authentication,
+  indexController.checkForValueInQuery,
+  indexController.subtraction
+);
 
-router.get('/multiply', indexController.multiplication);
+router.get(
+  '/multiply',
+  indexController.authentication,
+  indexController.checkForValueInQuery,
+  indexController.multiplication
+);
 
-router.get('/divide', indexController.division);
+router.get(
+  '/divide',
+  indexController.authentication,
+  indexController.checkForValueInQuery,
+  indexController.division
+);
 
-router.get('/history', indexController.history);
+router.get('/history', indexController.authentication, indexController.history);
 
 module.exports = router;
