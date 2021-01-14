c8dbec29f16d493463031c02064e55b26d19cf9a
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-01-14 10:11:31 +0530

    Completed logic to controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,71 @@
-exports.authentication = (req, res) => {};
+const db = require('../database/index');
 
-exports.addition = (req, res) => {};
+exports.authentication = (req, res, next) => {
+  if (req.query.apiKey != process.env.AUTHENTICATION_KEY) {
+    return res.json({
+      message: 'Please provide valid API Key as ?apiKey=APIKEY',
+    });
+  }
+  return next();
+};
 
-exports.subtraction = (req, res) => {};
+exports.checkForValueInQuery = (req, res, next) => {
+  if (req.query.firstValue != null && req.query.secondValue != null) {
+    return next();
+  } else {
+    return res.json({
+      message:
+        'Provide values for operations as ?firstValue=VALUE&secondValue=VALUE',
+    });
+  }
+};
 
-exports.multiplication = (req, res) => {};
+exports.addition = async (req, res) => {
+  let result = parseInt(req.query.firstValue) + parseInt(req.query.secondValue);
+  try {
+    await db.execute(
+      `INSERT INTO \`history\` (\`id\`, \`operation\`) VALUES (NULL, '${req.query.firstValue} + ${req.query.secondValue} = ${result}');`
+    );
+    res.json({ result });
+  } catch (error) {
+    res.json({ message: error.sqlMessage });
+  }
+};
 
-exports.division = (req, res) => {};
+exports.subtraction = async (req, res) => {
+  let result = parseInt(req.query.firstValue) - parseInt(req.query.secondValue);
+  try {
+    await db.execute(
+      `INSERT INTO \`history\` (\`id\`, \`operation\`) VALUES (NULL, '${req.query.firstValue} - ${req.query.secondValue} = ${result}');`
+    );
+    res.json({ result });
+  } catch (error) {
+    res.json({ message: error.sqlMessage });
+  }
+};
+
+exports.multiplication = async (req, res) => {
+  let result = parseInt(req.query.firstValue) * parseInt(req.query.secondValue);
+  try {
+    await db.execute(
+      `INSERT INTO \`history\` (\`id\`, \`operation\`) VALUES (NULL, '${req.query.firstValue} * ${req.query.secondValue} = ${result}');`
+    );
+    res.json({ result });
+  } catch (error) {
+    res.json({ message: error.sqlMessage });
+  }
+};
+
+exports.division = async (req, res) => {
+  let result = parseInt(req.query.firstValue) / parseInt(req.query.secondValue);
+  try {
+    await db.execute(
+      `INSERT INTO \`history\` (\`id\`, \`operation\`) VALUES (NULL, '${req.query.firstValue} / ${req.query.secondValue} = ${result}');`
+    );
+    res.json({ result });
+  } catch (error) {
+    res.json({ message: error.sqlMessage });
+  }
+};
 
 exports.history = (req, res) => {};
