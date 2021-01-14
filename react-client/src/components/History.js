f4c5aa7b34b9c746829ac593e4f3c94264483523
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-01-14 18:51:21 +0530

    Added logic for pagination in history

--- react-client/src/components/History.js
+++ react-client/src/components/History.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import { useState } from 'react';
+
+import Pagination from './Pagination';
 
 const History = ({ history, loading }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  let currentOperations = [];
+  const operationsPerPage = 10;
+
+  // Get current operation history
+  const indexOfLastOperation = currentPage * operationsPerPage;
+  const indexOfFirstOperation = indexOfLastOperation - operationsPerPage;
+  currentOperations = history.slice(
+    indexOfFirstOperation,
+    indexOfLastOperation
+  );
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
   if (loading) {
     return (
       <div className='container'>
@@ -20,7 +37,7 @@ const History = ({ history, loading }) => {
         <li className='list-group-item d-flex justify-content-center align-items-center'>
           <h4>History</h4>
         </li>
-        {history.map((operation, index) => (
+        {currentOperations.map((operation, index) => (
           <li
             key={index}
             className='list-group-item d-flex justify-content-center align-items-center'
@@ -29,6 +46,14 @@ const History = ({ history, loading }) => {
           </li>
         ))}
       </ul>
+      <div className='row justify-content-center my-3'>
+        <Pagination
+          operationsPerPage={operationsPerPage}
+          totalOperations={history.length}
+          paginate={paginate}
+          currentPage={currentPage}
+        />
+      </div>
     </div>
   );
 };
